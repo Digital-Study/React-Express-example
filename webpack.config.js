98a43b6d931f78628fa82726c06e06e9ff0772b3
akamksha-k <603005@PC285850.cts.com>
2017-04-28 12:55:59 +0530

    untracked files

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+module.exports = {
+    context: path.join(__dirname, 'src'),
+  entry:[ 
+      './client.js'
+      ],
+  output: {
+     
+    path:  path.join(__dirname, 'public'),
+    filename: 'bundle.js'       
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.json'] 
+  }
+};
